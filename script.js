3bae80312c67927863e07d8eba8a313fad0aa5cd
Nickollas <nickollassilva90@gmail.com>
2024-05-23 11:37:24 -0300

    #

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-const containerVideos = document.querySelector('.videos__container')
-const containerPdfs = document.querySelector('.pdfs__container')
+const containerVideos = document.querySelector(".videos__container");
+const containerPdfs = document.querySelector(".pdfs__container");
 
 async function buscarEMostrarVideos() {
-    try {
-        const busca = await fetch("https://raw.githubusercontent.com/NickProfessor/videos-do-nick/main/backend/videos.json");
-        if (!busca.ok) {
-            throw new Error("Erro ao carregar os vídeos: " + busca.status);
-        }
+  try {
+    const busca = await fetch(
+      "https://raw.githubusercontent.com/NickProfessor/videos-do-nick/main/backend/videos.json"
+    );
+    if (!busca.ok) {
+      throw new Error("Erro ao carregar os vídeos: " + busca.status);
+    }
 
-        const data = await busca.json();
-        let videos;
-        // Verifica se a resposta é um objeto com uma chave de vídeos
-        if (data && data.videos && Array.isArray(data.videos)) {
-            videos = data.videos;
-        } else if (Array.isArray(data)) { // Verifica se a resposta é diretamente uma matriz
-            videos = data;
-        } else {
-            throw new Error("Os vídeos não estão em um formato válido.");
-        }
+    const data = await busca.json();
+    let videos;
+    // Verifica se a resposta é um objeto com uma chave de vídeos
+    if (data && data.videos && Array.isArray(data.videos)) {
+      videos = data.videos;
+    } else if (Array.isArray(data)) {
+      // Verifica se a resposta é diretamente uma matriz
+      videos = data;
+    } else {
+      throw new Error("Os vídeos não estão em um formato válido.");
+    }
 
-        videos.forEach((video) => {
-            if (!video.categoria) {
-                throw new Error("Vídeo sem categoria.");
-            }
-            containerVideos.innerHTML += `
+    videos.forEach((video) => {
+      if (!video.categoria) {
+        throw new Error("Vídeo sem categoria.");
+      }
+      containerVideos.innerHTML += `
             <li class="videos__item">
                 <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
@@ -34,52 +37,50 @@ async function buscarEMostrarVideos() {
                 </div>
             </li>
             `;
-        });
-    } catch(error) {
-        containerVideos.innerHTML += `<p style="background-color: red;">Houve um erro ao carregar os vídeos: ${error.message}</p>`;
-    } finally {
-        
-    }
+    });
+  } catch (error) {
+    containerVideos.innerHTML += `<p style="background-color: red;">Houve um erro ao carregar os vídeos: ${error.message}</p>`;
+  } finally {
+  }
 }
 
 async function buscarEMostrarPdfs() {
-    try {
-        const busca = await fetch("https://raw.githubusercontent.com/NickProfessor/videos-do-nick/main/backend/pdfs.json")
-        if (!busca.ok) {
-            throw new Error("Erro ao carregar os pdfs: " + busca.status);
-        }
+  try {
+    const busca = await fetch(
+      "https://raw.githubusercontent.com/NickProfessor/videos-do-nick/main/backend/pdfs.json"
+    );
+    if (!busca.ok) {
+      throw new Error("Erro ao carregar os pdfs: " + busca.status);
+    }
 
-        const data = await busca.json();
-        let pdfs;
-        // Verifica se a resposta é um objeto com uma chave de vídeos
-        if (data && data.pdfs && Array.isArray(data.pdfs)) {
-            pdfs = data.pdfs;
-        } else if (Array.isArray(data)) { // Verifica se a resposta é diretamente uma matriz
-            pdfs = data;
-        } else {
-            throw new Error("Os pdfs não estão em um formato válido.");
-        }
+    const data = await busca.json();
+    let pdfs;
+    // Verifica se a resposta é um objeto com uma chave de vídeos
+    if (data && data.pdfs && Array.isArray(data.pdfs)) {
+      pdfs = data.pdfs;
+    } else if (Array.isArray(data)) {
+      // Verifica se a resposta é diretamente uma matriz
+      pdfs = data;
+    } else {
+      throw new Error("Os pdfs não estão em um formato válido.");
+    }
 
-        pdfs.forEach((pdf) => {
-            if (!pdf.url) {
-                throw new Error("Vídeo sem url.");
-            }
-            containerPdfs.innerHTML += `
+    pdfs.forEach((pdf) => {
+      if (!pdf.url) {
+        throw new Error("Vídeo sem url.");
+      }
+      containerPdfs.innerHTML += `
                 <a link href="${pdf.url}" target="_blank" class="pdfs__item">    
                     <img src="https://cdn-icons-png.flaticon.com/512/337/337946.png" alt="icone de pdf">   
                         <h3 class="titulo-pdf">${pdf.titulo}</h3>
                 </a>
             `;
-        });
-    } catch(error) {
-        containerPdfs.innerHTML += `<p style="background-color: red;">Houve um erro ao carregar os pdfs: ${error.message}</p>`;
-    } finally {
-        
-    }
+    });
+  } catch (error) {
+    containerPdfs.innerHTML += `<p style="background-color: red;">Houve um erro ao carregar os pdfs: ${error.message}</p>`;
+  } finally {
+  }
 }
-
-
-
 
 // function bemVindo(){
 //     let nome
